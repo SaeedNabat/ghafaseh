43deb3bb67c70532697b056f7861d647f68d4f5f
saeed <saeednabati@hotmail.com>
2022-07-28 19:47:30 +0430

    main page till footer

--- js/main.js
+++ js/main.js
@@ -5,4 +5,20 @@ $(document).ready(function() {
     $('#navbar-categories').blur(function() {
         $('#categories-dropdown').removeClass('active');
     })
+    $('.previous').first().click(function(e) {
+        $('.slider-item').last().addClass('animate__slideOutRight hide');
+        setTimeout(function() {
+            $('.slider-item').first().addClass('show animate__slideInLeft');
+            $('.slider-item').first().removeClass('hide animate__slideOutLeft');
+            $('.slider-item').last().removeClass('show animate__slideInRight');
+        }, 200);
+    });
+    $('.next').first().click(function(e) {
+        $('.slider-item').first().addClass('animate__slideOutLeft hide');
+        setTimeout(function() {
+            $('.slider-item').first().removeClass('show animate__slideInLeft');
+            $('.slider-item').last().addClass('show animate__slideInRight');
+            $('.slider-item').last().removeClass('hide animate__slideOutRight');        
+        },200)
+    });
 });
